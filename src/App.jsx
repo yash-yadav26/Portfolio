12a5c50aa1e06635486bdb00edb9fcfe93298fe7
yash yadav <yashyadav73000@gmail.com>
2025-07-03 17:44:34 +0530

    done!!

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Home from './components/Home'
+import About from './components/About'
+import PortFolio from './components/Portfolio'
+import Experiance from './components/Experience'
+import Footer from './components/Footer'
+import Contact from './components/Contact'
+import {Toaster} from 'react-hot-toast'
+
+const App = () => {
+  return (
+ <>
+ <div>
+ <Navbar/>
+ <Home/>
+ <About/>
+ <PortFolio/>
+ <Experiance/>
+ <Contact/>
+ <Footer/>
+ </div>
+ <Toaster/>
+ </>
+  )
+}
+
+export default App
